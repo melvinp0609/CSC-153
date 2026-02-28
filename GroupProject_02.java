63c3a4a596e7ddf28d4fe96d4906c08e9c81eb75
melvinp0609 <melvinp0609@student.faytechcc.edu>
2026-02-28 17:22:54 -0500

    Update GroupProject_02.java

--- GroupProject_02.java
+++ GroupProject_02.java
@@ -2,6 +2,7 @@
 // Alyssa Young (Aly3673) - Contributed file I/O system and editing function.
 // Bryanna Wilson (wilsonb2742) - Contributed file I/O system, list layout for the main panel, detail panel function, and CSV files with player and staff information.
 // Prince Melvin (melvinp0609) - Help autosave anydata that is written
+
 import javax.swing.*;
 import java.io.*;
 import java.awt.*;
